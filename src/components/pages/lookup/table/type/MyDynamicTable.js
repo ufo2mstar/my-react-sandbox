e730faccf530186b3f99130dca8f9c9696e508dd
Naren SivaSubramani <ufo2mstar@gmail.com>
2018-03-08 16:47:04 -0500

    APP: WIP: addons and grid sort done

--- src/components/pages/lookup/table/type/MyDynamicTable.js
+++ src/components/pages/lookup/table/type/MyDynamicTable.js
@@ -1,24 +1,40 @@
 import React from 'react'
 // import BootstrapTable from 'react-bootstrap-table-next';
 import ReactDataGrid from 'react-data-grid';
+import ReactDataGridPlugins from 'react-data-grid'
 
 import products from './data'
 
 class ProductList extends React.Component {
   constructor(props, context) {
     super(props, context);
-    this.state = {productList: products};
-    this._columns = Object.keys(this.state.productList[0]).map((key, index) => (
+    this.state = {rows: products, originalRows: products};
+    this._columns = Object.keys(this.state.rows[0]).map((key, index) => (
       {
         key: key,
         name: key,
-        sort: true
+        sortable: true,
+        filterable: true
       }
     ));
   }
 
   rowGetter = (i) => {
-    return this.state.productList[i];
+    return this.state.rows[i];
+  };
+
+  handleGridSort = (sortColumn, sortDirection) => {
+    const comparer = (a, b) => {
+      if (sortDirection === 'ASC') {
+        return (a[sortColumn] > b[sortColumn]) ? 1 : -1;
+      } else if (sortDirection === 'DESC') {
+        return (a[sortColumn] < b[sortColumn]) ? 1 : -1;
+      }
+    };
+
+    const rows = sortDirection === 'NONE' ? this.state.originalRows.slice(0) : this.state.rows.sort(comparer);
+
+    this.setState({ rows });
   };
 
   render() {
@@ -26,8 +42,10 @@ class ProductList extends React.Component {
       <ReactDataGrid
         columns={this._columns}
         rowGetter={this.rowGetter}
-        rowsCount={this.state.productList.length}
-        minHeight={500}/>);
+        rowsCount={this.state.rows.length}
+        //sort
+        onGridSort={this.handleGridSort}
+        minHeight={400}/>);
   }
 }
 
